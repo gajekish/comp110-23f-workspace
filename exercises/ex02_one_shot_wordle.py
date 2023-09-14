947e2b56913564f4579c629a0b928beed45285d9
Kishan Gajera <gajekish@unc.edu>
2023-09-14 15:29:11 -0400

    One Shot Wordle Version 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""EX02 - One-Shot Wordle"""
+"""EX02 - One-Shot Wordle."""
 __author__ = "730662291"
 
 WHITE_BOX: str = "\U00002B1C"
@@ -9,20 +9,27 @@ secret_word: str = "python"
 word_guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 
 while (len(word_guess) != len(secret_word)):
-    word_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
+    word_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
 
 index: int = 0
 guess_outline: str = ""
 while (index < len(secret_word)):
     if (secret_word[index] == word_guess[index]):
-        guess_outline += GREEN_BOX + " "
-    elif (word_guess[index] in secret_word):
-        guess_outline += YELLOW_BOX + " "
+        guess_outline += GREEN_BOX
     else:
-        guess_outline += WHITE_BOX + " "
+        guess_in_word: bool = False
+        inner_index: int = 0
+        while (guess_in_word != True and inner_index < len(secret_word)):
+            if (word_guess[index] == secret_word[inner_index]):
+                guess_in_word = True
+            else:
+                inner_index += 1
+        if (guess_in_word): 
+            guess_outline += YELLOW_BOX
+        else:
+            guess_outline += WHITE_BOX
     index += 1
 print(guess_outline)
-
 
 if (word_guess == secret_word):
     print("Woo! You got it!")
