38927fd9111e39468a4c680ca72fc9a422760c73
Kishan Gajera <gajekish@unc.edu>
2023-09-22 17:24:43 -0400

    Defined Function with Input

--- lessons/functions.py
+++ lessons/functions.py
@@ -1,5 +1,7 @@
 """Example Functions"""
 
+num1: int = int(input("Enter a number: "))
+num2: int = int(input("Enter another number: "))
 def my_max(number1: int, number2: int):
     """Returns the maximum value out of two numbers"""
     if (number1 >= number2):
@@ -7,7 +9,7 @@ def my_max(number1: int, number2: int):
     else: #number 1 < number 2
         return number2
     
-max_number: int = my_max(1,10)
+max_number: int = my_max(num1, num2)
 other_max_number: int = my_max(11,10)
 print(max_number)
 print(other_max_number)
