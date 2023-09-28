1d244fc2b6abfe1b0ba8216ef3fafe74220bf4df
Kishan Gajera <gajekish@unc.edu>
2023-09-28 09:42:52 -0400

    Understanding Imports

---
+++ lessons/import.py
@@ -0,0 +1,9 @@
+"""Practicing imports from other modules"""
+
+from lessons import my_functions
+
+print(my_functions.addition(1,2))
+print(my_functions.my_variable)
+
+if __name__ == "__main__":
+    print("Howdy!")
